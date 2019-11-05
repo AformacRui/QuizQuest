8763e74fc0deea93a51c19bf4463d265cd50d23d
Hermann <hermannhouedanou@gmail.com>
2019-11-05 16:55:06 +0100

    page index fait

---
+++ index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!--Nav-container Script & link-->
+        
+        
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">QuizQuest</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#!" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Connexion</a></li>
+                        <li><a href="#!" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Créer un Quiz</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+        <div class="container-fluid d-flex">
+            <ul id="menu-nav" class="align-self-center mr-3">
+                <h1>Thèmes</h1>
+            <li><a href="#">Maths</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Cinéma</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Sport</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+
+            <li><a href="#">Musique</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Histoire</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Géographie</a>
+                <ul>
+                    <li><a href="#">Quizz 1</a></li>
+                    <li><a href="#">Quizz 2</a></li>
+                    <li><a href="#">Quizz 3</a></li>
+                    <li><a href="#">Quizz 4</a></li>
+                </ul>
+            </li>
+        </ul>
+
+
+        <div class="main-container">
+            <div class="row">
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">1</div>
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">2</div>
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">3</div>
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">4</div>
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">5</div>
+                <div class="col-xs-10 col-sm-3 col-lg-4 list-group-item d-flex justify-content-between align-items-center">6</div>
+            </div>
+        </div> <!-- #main-container -->
+        
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h3>footer</h3>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
+        <script src="js/main.js"></script>
+        
+
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
