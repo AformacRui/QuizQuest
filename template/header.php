f992dfa4ce76c50c3eec2a0e7be9cbcad34d6125
Hermann <hermannhouedanou@gmail.com>
2019-11-08 15:59:06 +0100

    ajax1

--- template/header.php
+++ template/header.php
@@ -1,11 +1,11 @@
 <div class="header-container">
-            <header class="wrapper clearfix">
-            <a href="../index.php"><h1 class="title">QuizQuest</h1></a>
-                <nav class="d-inline">
-                    <ul class="flex-column flex-sm-row">
-                        <li><a href="../scripts_php/login.php" class="btn btn-default btn-lg" role="button" aria-pressed="true"><p>Connexion</p></a></li>
-                        <li><a href="../scripts_php/insert_quiz.php" class="btn btn-default btn-lg" role="button" aria-pressed="true"><p>Q_Créateur</p></a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+    <header class="wrapper clearfix">
+    <a href="../index.php"><h1 class="title">QuizQuest</h1></a>
+        <nav class="d-inline">
+            <ul class="flex-column flex-sm-row">
+                <li><a href="../scripts_php/login.php" class="btn btn-default btn-lg" role="button" aria-pressed="true"><p>Connexion</p></a></li>
+                <li><a href="../scripts_php/insert_quiz.php" class="btn btn-default btn-lg" role="button" aria-pressed="true"><p>Q_Créateur</p></a></li>
+            </ul>
+        </nav>
+    </header>
+</div>
--- template/quizchoice.php
+++ template/quizchoice.php
@@ -1,27 +1,9 @@
-<?php
-include("../scripts_php/head.php");
-?>
-
-<body>
-    <?php include("../template/header.php");?>
-
-    <div class="container-fluid d-flex ">
-            
-            <?php include("../template/main_nav.php"); ?>
-
-
-        <div class="main-container" id='validChoice'>
-            <div class="question"><h1>VOULEZ VOUS JOUER A CE QUIZZ ?</h1></div>
-            <div class="response">
-                <button>OUI</button>
-                <button>NON</button>
-            </div>
-        </div> <!-- #main-container -->
-        
-        </div>
-        <div class="footer-container">
-        <?php include("../template/footer.php"); ?>
-
+ <div class="row">
+    <div class="question">
+        <h1>VOULEZ VOUS JOUER A CE QUIZZ ?</h1>
     </div>
-</body>
-</html>
+    <div class="reponse">
+        <button>OUI</button>
+        <button>NON</button>
+    </div> 
+</div>   
