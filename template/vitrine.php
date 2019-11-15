473fb93b9e144ba2ff816b6b3ffef1dc32984411
Hermann <hermannhouedanou@gmail.com>
2019-11-15 10:22:07 +0100

    vitrine refait

--- template/vitrine.php
+++ template/vitrine.php
@@ -1,12 +1,11 @@
 
-<ul class="list-group" style="d-flex; flex-row"> 
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id="clickChoice1" style="background-color:red;"> Case 1</div></li>
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id='clickChoice2'> Case 2</div></li>
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id='clickChoice3'> Case 3</div></li>
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id='clickChoice4'> Case 4</div></li>
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id='clickChoice5'> Case 5</div></li>
-    <li><div class="col-xs-12 col-sm-6 col-lg-3 list-group-item d-flex justify-content-between align-items-center" id='clickChoice6'> Case 6</div></li>
-</ul>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id="clickChoice1"> Case 1</div>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id='clickChoice2'> Case 2</div>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id='clickChoice3'> Case 3</div>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id='clickChoice4'> Case 4</div>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id='clickChoice5'> Case 5</div>
+<div class="col-xs-12 col-sm-6 col-lg-4 list-group-item d-flex justify-content-between align-items-center" id='clickChoice6'> Case 6</div>
+
 <!--
 <ul class="list-group" style="">
     <li class="col-xs-12 col-sm-6 col-lg-4 list-group-item" id="clickChoice1"> Casier 1</li>
