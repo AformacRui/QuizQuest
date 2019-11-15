01c5abfe5576f5440bde52a30dab5f57efae150c
Hermann <hermannhouedanou@gmail.com>
2019-11-15 12:05:45 +0100

    new change

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
-<?php include("scripts_php/head.php");
-include("scripts_php/connexion.php");
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-<<<<<<< HEAD
         <link rel="stylesheet" href="css/normalize.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
         
-=======
- ?>
->>>>>>> abda4d5af3903e4c5153d73f41b343a3c1f76375
 
+        <!--Nav-container Script & link-->
+        
+        
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
-
         <?php 
            session_start();
            if(isset($_SESSION['userName'])) {
              echo "Your session is running " . $_SESSION['userName'];
            }
-           else{
-            $_SESSION['userName'] = "visitor";
-           }
         
-           
         include("template/header.php");?>
 
         <div class="container-fluid d-flex ">
@@ -44,23 +52,23 @@ include("scripts_php/connexion.php");
             <?php include("template/footer.php"); ?>
         </div>
 
-       <!--  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script> -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-        <!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
-        <?php include("scripts_php/scripts.php");?>
+        <script src="js/main.js"></script>
         
 
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-         <script>
+        <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
             e.src='//www.google-analytics.com/analytics.js';
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        
+        </script>
     </body>
 </html>
