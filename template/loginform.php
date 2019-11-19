18133a1be51e38dfdf615f691681f1bbdb7cbff0
Hermann <hermannhouedanou@gmail.com>
2019-11-19 22:15:55 +0100

    admin page

--- template/loginform.php
+++ template/loginform.php
@@ -1,5 +1,5 @@
 <form action="verify_login.php" method="post" class="form_user">
-                    <h6>Your Nickname:</h6> <input type="text" name="nicklog"><br>
-                    <h6>PassWord:</h6> <input type="text" name="passlog"><br><br>
-                    <input type="submit">
-                </form>
+    <h6>Your Nickname:</h6> <input type="text" name="nicklog"><br>
+    <h6>PassWord:</h6> <input type="text" name="passlog"><br><br>
+    <input type="submit">
+</form>
