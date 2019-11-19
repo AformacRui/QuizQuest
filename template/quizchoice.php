41e85e489279c48f6efcb53a7f35e0715c5b5e71
Hermann <hermannhouedanou@gmail.com>
2019-11-19 10:28:21 +0100

    up

--- template/quizchoice.php
+++ template/quizchoice.php
@@ -3,7 +3,7 @@
         <h1>VOULEZ VOUS JOUER A CE QUIZZ ?</h1>
     </div>
     <div class="reponse m-2">
-        <a href="index.php"><button class='btn btn-primary btn-lg active'>OUI</button></a>
-        <button class='btn btn-primary btn-lg active'>NON</button>
+        <a href="../scripts_php/response.php"><button class='btn btn-primary btn-lg active'>OUI</button></a>
+        <a href="index.php"><button class='btn btn-primary btn-lg active'>NON</button></a>
     </div> 
 </div>
