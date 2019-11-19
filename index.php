703d954f22815256d2d1a22f9a90ccc47b9361e9
Coura <afor.rui@gmail.com>
2019-11-19 11:25:25 +0100

    origin

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php include("scripts_php/head.php");
 include("scripts_php/connexion.php");
 
-<<<<<<< HEAD
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/main.css">
-        
-=======
- ?>
->>>>>>> abda4d5af3903e4c5153d73f41b343a3c1f76375
+?>
 
     <body>
         <!--[if lt IE 8]>
@@ -52,15 +45,6 @@ include("scripts_php/connexion.php");
         <?php include("scripts_php/scripts.php");?>
         
 
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-         <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         
     </body>
 </html>
